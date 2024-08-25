a700a42926083b979dec43b78fede62e1b9ff8bf
Jens Gwerder <jens.gwerder@protonmail.ch>
2024-08-25 21:45:34 +0200

    Align upgradable package info output for consistent formatting

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def main():
     logger.info("Logger is set up")
 
     packages_to_update = package_handler.get_upgradable_packages()
+    max_package_name_length = max(len(package.package_name) for package in packages_to_update)
+    max_package_current_version = max(len(package.current_version) for package in packages_to_update)
+    max_package_new_version = max(len(package.new_version) for package in packages_to_update)
 
     if not packages_to_update:
         logger.info("No packages to upgrade")
@@ -19,6 +22,12 @@ def main():
 
     logger.info(f"Upgradable packages ({len(packages_to_update)}):")
     logger.info("--------------------")
+    for package in packages_to_update:
+        logger.info(f"{package.package_name.ljust(max_package_name_length)} "
+                    f"{package.current_version.ljust(max_package_current_version)} -> "
+                    f"{package.new_version.ljust(max_package_new_version)}")
+    logger.info("--------------------")
+
     for package in packages_to_update:
         logger.info(f"{package.package_name} {package.current_version} -> {package.new_version}")
         package_changelog = package_handler.get_package_changelog(package) # TODO Check if it returns False
