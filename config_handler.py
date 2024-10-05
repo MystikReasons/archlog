de074c1b129bb13a29793af1d10b03b222083cb5
Jens Gwerder <jens.gwerder@protonmail.ch>
2024-10-05 14:30:48 +0200

    Added functionality to save package changelogs in a JSON file

--- config_handler.py
+++ config_handler.py
@@ -14,12 +14,15 @@ class ConfigHandler:
 
         self.datetime_now = datetime.now()
         self.dt_string_logging = self.datetime_now.strftime("%d-%m-%Y_%H-%M-%S")
-        self.dt_string = self.datetime_now.strftime("%d.%m.%Y")
+        self.dt_string_changelog = self.datetime_now.strftime("changelog-%d-%m-%Y.json")
 
         self.logs_path = os.path.join(self.dir_path, "logs", "")
-
         if not os.path.exists(self.logs_path):
             os.makedirs(self.logs_path)
+
+        self.changelog_path = os.path.join(self.dir_path, "changelog", "")
+        if not os.path.exists(self.changelog_path):
+            os.makedirs(self.changelog_path)
 
         self.setup_logging()
 
@@ -59,7 +62,46 @@ class ConfigHandler:
             return None
         return config
 
-    def write_changelog(self, package_changelog: List[Tuple[str, str]]):
+    def write_changelog(self, package, package_changelog: List[Tuple[str, str, str]]):
+        changelog_filename=self.changelog_path + self.dt_string_changelog
+
+        if os.path.exists(changelog_filename):
+            with open(changelog_filename, "r") as json_read_file:
+                try:
+                    existing_data = json.load(json_read_file)
+                except json.JSONDecodeError:
+                    existing_data = {}
+        else:
+            existing_data = {}
+
+        if package.package_name not in existing_data:
+            existing_data[package.package_name] = {
+                "current version": package.current_version,
+                "new version": package.new_version,
+                "versions": []
+            }
+
+        versions_dict = {}
+        
+        if package_changelog:
+            for commit_message, commit_url, commit_tag in package_changelog:
+                if commit_tag not in versions_dict:
+                    versions_dict[commit_tag] = []
+                
+                versions_dict[commit_tag].append({
+                    "commit message": commit_message,
+                    "commit URL": commit_url
+                })
+        else:
+            version_tag = package.current_version
+            versions_dict[version_tag] = []
+        
+        for versionTag, changelog in versions_dict.items():
+            existing_data[package.package_name]["versions"].append({
+                "version-tag": versionTag,
+                "changelog": changelog
+            })
+
         # Write the updated website file data back to the file
-        with open(website_collection_path, "w") as write_file:
-            json.dump(website_file_data, write_file, indent=4)
+        with open(changelog_filename, "w") as json_write_file:
+            json.dump(existing_data, json_write_file, indent=4)
